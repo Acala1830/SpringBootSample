8b4d530c896842da293a99904cb60038a5d8f2b5
Willosa <willosa1830@gmail.com>
2025-02-04 17:35:02 +0900

    6章3（バリデーション、エラーメッセージの編集と順番）

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.form.GroupOrder;
 import com.example.demo.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -39,7 +41,7 @@ public class SignupController {
 	/** ユーザー登録処理 */
 	@PostMapping("/signup")
 	public String postSignup(Model model, Locale locale, 
-			@ModelAttribute SignupForm form,
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm form,
 			BindingResult bindingResult) {
 		
 		// 入力チェック結果
